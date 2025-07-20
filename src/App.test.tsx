5e20a1daec69bbdaafb7bf9490a0538ea936e7e2
Eugeku <eugenekuzora@gmail.com>
2025-07-20 20:54:07 +0200

    fix: fix allises

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,8 +10,8 @@ import {
 } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from './App';
-import type { Book } from './types';
+import App from '@/App';
+import type { Book } from '@/types';
 
 vi.mock('@components/CardList', () => {
   interface Props {
